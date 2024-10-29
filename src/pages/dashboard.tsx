9988cee7820408e46e01c1cf812fc497572e55ef
Georgios <geopapad9@gmail.com>
2024-10-29 23:48:58 +0000

    typo

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,7 +2,7 @@ export default function Dashboard() {
   return (
     <div style={{ padding: "24px", textAlign: "center" }}>
       <h1>Dashboard</h1>
-      <p>View team and individual player statistics will go here.</p>
+      <p>View team and individual player statistics.</p>
     </div>
   );
 }
